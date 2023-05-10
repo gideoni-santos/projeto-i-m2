f8dbe814619fe9e466e663da3bd28b9091978b59
Gideoni <115567537+gideoni-santos@users.noreply.github.com>
2023-05-10 20:45:10 -0300

    criação de variáveis
    criação das variáveis solicitadas e definição da lógica do programa.

---
+++ main.py
@@ -0,0 +1,13 @@
+candidatos = []
+
+# while True:
+nome = input("Nome do candidato(a): ")
+entrevista = float(input("Nota da entrevista: "))
+teorico = float(input("Nota do teste teórico: "))
+pratico = float(input("Nota do teste prático: "))
+soft = float(input("Nota da avaliação de Soft Skills: "))
+
+nome.append(candidatos)
+teorico.append([])
+    
+    
