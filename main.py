b8daf4ae5890bb4ae09aef82f9cae3bb5fee0b45
Gideoni <115567537+gideoni-santos@users.noreply.github.com>
2023-05-10 21:05:29 -0300

    colocando os inputs dentro da lista
    fiz com que os inputs digitados entrassem na lista vazia "candidatos", sendo que as notas estão em uma sublista.

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 candidatos = []
 
-# while True:
-nome = input("Nome do candidato(a): ")
-entrevista = float(input("Nota da entrevista: "))
-teorico = float(input("Nota do teste teórico: "))
-pratico = float(input("Nota do teste prático: "))
-soft = float(input("Nota da avaliação de Soft Skills: "))
+while True:
+    nome = input("Nome do candidato(a): ")
+    entrevista = float(input("Nota da entrevista: "))
+    teorico = float(input("Nota do teste teórico: "))
+    pratico = float(input("Nota do teste prático: "))
+    soft = float(input("Nota da avaliação de Soft Skills: "))
 
-nome.append(candidatos)
-teorico.append([])
+    notas = [nome, [entrevista, teorico, pratico, soft]]
+    candidatos.append(notas)
+    
+    print(candidatos)
+
+
     
     
