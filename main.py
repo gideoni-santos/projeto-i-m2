c68ffd2548ca90ec650324d866d62ceb013dddee
Gideoni <115567537+gideoni-santos@users.noreply.github.com>
2023-05-11 21:03:42 -0300

    Finalizado opcao mostrar candidatos
    finalizei a opção 3, ver candidatos cadastrados. Como também formatei usando índices para imprimir de uma forma mais fácil de ler a lista "candidatos"

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ while True:
             opcaoCandidatos = input(
                 "\n[1] Cadastrar novo candidato\n"
                 "[2] Verificar compatibilidade de candidatos\n"
-                "[3] Ver candidatos\n"
+                "[3] Ver candidatos cadastrados\n"
                 "[4] Voltar ao menu aterior\n"
                 "[5] Encerrar programa\n"
                 "\nInsira a opção desejada: "
@@ -31,11 +31,18 @@ while True:
                     notas = [nome, [entrevista, teorico, pratico, soft]]
                     candidatos.append(notas)
                     print("\nUsúario cadastrado com sucesso!")
-                    
+                    break  
             elif opcaoCandidatos == "2": #ENTRA NA VERIFICAÇÃO DE CANDIDATOS
                 print("opcao2")
             elif opcaoCandidatos == "3": #MOSTRA OS CANDIDATOS CADASTRADOS NA LISTA "CANDIDATOS"
-                print(candidatos)
+                for candidato in candidatos: #MOSTRA A LISTA DE CANDIDATOS MAIS FÁCIL DE LER
+                    print(f"\nNome: {candidato[0]}") #BUSCA O INDEX DO NOME DO CANDIDATO "0"
+                    print(
+                        f"Entrevista - {candidato[1][0]}\n" # BUSCA O INDICE DA SUBLISTA DE ACORDO COM A ORDEM DA NOTA
+                        f"Teórico - {candidato[1][1]}\n"
+                        f"Prático - {candidato[1][2]}\n"
+                        f"Soft Skills - {candidato[1][3]}"
+                    )
             elif opcaoCandidatos == "4": #VOLTA AO MENU ANTERIOR
                 break
             elif opcaoCandidatos == "5": #SAIR DO PROGRAMA
@@ -52,8 +59,6 @@ while True:
         
     else:
         print("Por favor Insira uma opção válida")
-    
-        
 
     
     
